3a530462161129b87c645fa3a68e8b3b259189ba
liu Meiyang <1084541514@qq.com>
2019-10-21 23:47:52 +0800

    C状态机模块编译通过

--- testC/states/module.c
+++ testC/states/module.c
@@ -1,7 +1,11 @@
-#include "moduel.h"
-void module_add_state(module_t* module,state* state)
+#include "module.h"
+void module_state_run(state_t* state);
+void subModule_list_process(module_t* module);
+
+void module_add_state(module_t* module,state_t* state)
 {
-	state_t* stateTemp, statePre;
+	state_t* stateTemp, *statePre;
+	state->module=module;
 	stateTemp=module->stateHead;
 	//头节点空
 	if(stateTemp==NULL)
@@ -15,13 +19,13 @@ void module_add_state(module_t* module,state* state)
 		statePre=stateTemp;
 		stateTemp=stateTemp->nextState;
 	}
-	//找到链表尾
+	//找到链表尾,增加状态
 	statePre->nextState=state;
 }
-void state_add_subModule(state* state,module_* subModule)
+void state_add_subModule(state_t* state,module_t* subModule)
 {
-	module_t* subModuleTemp, subModulePre;
-	subModuleTemp=state->subModuleHead; 
+	module_t* subModuleTemp, *subModulePre;
+	subModuleTemp=state->subModuleHead;
 	if(subModuleTemp==NULL)
 	{
 		state->subModuleHead=subModule;
@@ -32,11 +36,11 @@ void state_add_subModule(state* state,module_* subModule)
 		subModulePre=subModuleTemp;
 		subModuleTemp=subModuleTemp->nextModule;
 	}
-	subModulePre->next=subModule;
+	subModulePre->nextModule=subModule;
 }
-void state_add_jump(state* state,jump_t* jump);
+void state_add_jump(state_t* state,jump_t* jump)
 {
-	jump_t* jumpTemp,jumpPre;
+	jump_t* jumpTemp,*jumpPre;
 	jumpTemp=state->jumpHead;
 	if(jumpTemp==NULL)
 	{
@@ -48,8 +52,9 @@ void state_add_jump(state* state,jump_t* jump);
 		jumpPre=jumpTemp;
 		jumpTemp=jumpTemp->nextJump;
 	}
-	jumpPre->next=jump;
+	jumpPre->nextJump=jump;
 }
+
 
 void module_process(module_t* module)
 {
@@ -68,32 +73,32 @@ void module_process(module_t* module)
 		}
 		state=state->nextState;
 	}
-	
+
 }
 void module_state_run(state_t* state)
 {
-	module_t* subModule=state->subModuleHead; 
-	jump_t jump=state->jump;
-	subModule_list_process(subModuleHead);
+	module_t* subModule=state->subModuleHead;
+	jump_t* jump=state->jumpHead;
+	subModule_list_process(subModule);
 	//遍历跳转链表
 	while(jump!=NULL)
 	{
 		if(jump->condition==NULL)//未添加跳转函数，认为满足跳转条件
 		{
-			if(jump->run!==NULL)
+			if(jump->run!=NULL)
 			{
 				jump->run();//跳转前执行回调
 			}
-			state->module->stateCurrent=targetStateValue;//执行跳转
+			state->module->stateCurrent=jump->targetStateValue;//执行跳转
 			break;
 		}
 		if(jump->condition())//满足跳转条件
 		{
-			if(jump->run!==NULL)
+			if(jump->run!=NULL)
 			{
 				jump->run();//跳转前执行回调
 			}
-			state->module->stateCurrent=targetStateValue;//执行跳转
+			state->module->stateCurrent=jump->targetStateValue;//执行跳转
 			break;
 		}
 		jump=jump->nextJump;
--- testC/states/module.h
+++ testC/states/module.h
@@ -1,3 +1,9 @@
+#ifndef FILE_MODULE_H
+    #define FILE_MODULE_H
+#ifndef NULL
+    #define NULL (void*)0
+#endif
+#define DEBUG printf("FUNC:%S,FILE:%s,LINE:%d",__func__,__FILE__,__LINE__);
 typedef struct statejump jump_t;//状态跳转
 typedef struct state state_t;//状态
 typedef struct module module_t;//模块
@@ -7,15 +13,15 @@ struct statejump
 {
 	unsigned targetStateValue;//跳转目标状态值
 	condition_callback_t condition;//状态跳转条件
-	jumpRun_t run;//状态跳转
+	jumpRun_callback_t run;//状态跳转
 	jumpRun_callback_t *nextJump;//跳转链表下一个节点
 };
 struct state
 {
-	module*t module;//拥有该状态的模块
-	unsigned char value;//状态值
-	module*t subModuleHead;//该状态下可用子模块链表头
-	module*T jumpHead;//跳转链表头
+    unsigned char value;//状态值
+	module_t* module;//拥有该状态的模块
+	module_t* subModuleHead;//该状态下可用子模块链表头
+	module_t* jumpHead;//跳转链表头
 	state_t* nextState;//状态链表下一个节点
 };
 struct module
@@ -24,9 +30,10 @@ struct module
 	state_t* stateHead;//模块拥有的状态链表头
 	module_t* nextModule;//模块链表下一个节点
 };
-void module_add_state(module_t* module,state* state);
-void state_add_subModule(state* state,module_* subModule);
-void state_add_jump(state* state,jump_t* jump); 
+void module_add_state(module_t* module,state_t* state);
+void state_add_subModule(state_t* state,module_t* subModule);
+void state_add_jump(state_t* state,jump_t* jump);
 void module_process(module_t* module);
+#endif
 
 
